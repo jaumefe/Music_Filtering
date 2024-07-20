803f9c9ec1e2a8ce146f160b53f4ea8e918cd57c
jaumefe <jaumef.escrig96@gmail.com>
2024-07-20 22:22:24 +0200

    First tries with fyne. GUI test on listing all songs

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 
-	"github.com/jessevdk/go-flags"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
-var opts struct {
-	Config   string `short:"c" long:"config" description:"Provide json file to filter"`
-	Template bool   `short:"t" long:"template" description:"Generate a template json with all the artists included (By default, it's false)"`
-}
+// var opts struct {
+// 	Config   string `short:"c" long:"config" description:"Provide json file to filter"`
+// 	Template bool   `short:"t" long:"template" description:"Generate a template json with all the artists included (By default, it's false)"`
+// }
 
 type band struct {
 	Name  string   `json:"artist"`
@@ -28,129 +28,111 @@ type music struct {
 }
 
 func main() {
-	_, err := flags.ParseArgs(&opts, os.Args)
+
+	music := music{Playlist: "Music", Band: make([]band, 0)}
+	dir, err := os.ReadDir("Music")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	template := opts.Template
-	config := opts.Config
-
-	if template {
-		// Reading Music directory
-		music := music{Playlist: "Music", Band: make([]band, 0)}
-		dir, err := os.ReadDir("Music")
-		if err != nil {
-			log.Fatal(err)
+	for _, file := range dir {
+		singSong := strings.Split(file.Name()[:len(file.Name())-4], " - ")
+		fmt.Println(singSong)
+		if len(singSong) <= 1 {
+			fmt.Printf("Unable to parse %v\n", singSong)
+		} else {
+			singer, songs := singSong[0], singSong[1]
+			if (len(music.Band) == 0) || (music.Band[len(music.Band)-1].Name != singer) {
+				music.Band = append(music.Band, band{Name: singer})
+			}
+			music.Band[len(music.Band)-1].Songs = append(music.Band[len(music.Band)-1].Songs, songs)
 		}
+	}
 
-		for _, file := range dir {
-			singSong := strings.Split(file.Name(), " - ")
-			if len(singSong) <= 1 {
-				fmt.Printf("Unable to parse %v\n", singSong)
-			} else {
-				singer, songs := singSong[0], singSong[1]
-				if (len(music.Band) == 0) || (music.Band[len(music.Band)-1].Name != singer) {
-					music.Band = append(music.Band, band{Name: singer})
-				}
-				music.Band[len(music.Band)-1].Songs = append(music.Band[len(music.Band)-1].Songs, songs)
+	a := app.New()
+	w := a.NewWindow("Music List")
+	w.Resize(fyne.Size{Width: 1080, Height: 480})
+
+	// Función para obtener los nombres de los artistas
+	getArtists := func() []string {
+		var artists []string
+		for _, b := range music.Band {
+			artists = append(artists, b.Name)
+		}
+		return artists
+	}
+
+	// Función para obtener las canciones de un artista
+	getSongs := func(artist string) []string {
+		for _, b := range music.Band {
+			if b.Name == artist {
+				return b.Songs
 			}
 		}
-
-		content, err := json.MarshalIndent(music, "", "\t")
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		err = os.WriteFile("All_Music.json", content, 0644)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-	} else if config != "" {
-		// Opening and parsing json file for filtering songs
-		var filter music
-		jsonFile, err := os.Open(config)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer jsonFile.Close()
-
-		byteValue, _ := io.ReadAll(jsonFile)
-		json.Unmarshal(byteValue, &filter)
-
-		err = os.Mkdir(filter.Playlist, 0750)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		// Filtering the content from the json
-		for i := 0; i < len(filter.Band); i++ {
-			name := filter.Band[i].Name
-			songs := filter.Band[i].Songs
-			switch {
-			case songs[0] == "*":
-				os.Chdir("Music/")
-				match, err := filepath.Glob(fmt.Sprintf("%v - *", name))
-				os.Chdir("../")
-				if err != nil {
-					fmt.Println(err)
-				}
-				for _, j := range match {
-					src := fmt.Sprintf("Music/%v", j)
-					dst := fmt.Sprintf("%v/%v", filter.Playlist, j)
-					err := copyFile(src, dst)
-					if err != nil {
-						fmt.Println(err)
-					}
-				}
-			case songs[0] == "~":
-				continue
-			default:
-				for _, j := range songs {
-					os.Chdir("Music/")
-					match, err := filepath.Glob(fmt.Sprintf("%v - %v*", name, j))
-					os.Chdir("../")
-					if err != nil {
-						fmt.Println(err)
-					}
-					src := fmt.Sprintf("Music/%v", match[0])
-					dst := fmt.Sprintf("%v/%v", filter.Playlist, match[0])
-					err = copyFile(src, dst)
-					if err != nil {
-						fmt.Println(err)
-					}
-				}
-			}
-		}
-	} else {
-		fmt.Println("No filter in json format provided")
+		return []string{}
 	}
 
+	// Obtiene la lista de artistas
+	artists := getArtists()
+	artistSelect := widget.NewSelect(artists, nil)
+
+	// Crea una lista para mostrar las canciones
+	songList := widget.NewList(
+		func() int {
+			return 0 // Inicialmente vacía
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText("") // Inicialmente vacía
+		},
+	)
+
+	// Función para actualizar la lista de canciones
+	updateSongList := func(artist string) {
+		songs := getSongs(artist)
+		songList.Length = func() int {
+			return len(songs)
+		}
+		songList.UpdateItem = func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(songs[i])
+		}
+		songList.Refresh()
+	}
+
+	// Maneja la selección de un artista
+	artistSelect.OnChanged = func(artist string) {
+		updateSongList(artist)
+	}
+
+	content := container.NewBorder(artistSelect, nil, nil, nil, songList)
+	w.SetContent(content)
+
+	w.ShowAndRun()
 }
 
-func copyFile(src string, dst string) error {
-	source, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer source.Close()
+// func copyFile(src string, dst string) error {
+// 	source, err := os.Open(src)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer source.Close()
 
-	destination, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer destination.Close()
+// 	destination, err := os.Create(dst)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer destination.Close()
 
-	_, err = io.Copy(destination, source)
-	if err != nil {
-		return err
-	}
+// 	_, err = io.Copy(destination, source)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	err = destination.Sync()
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// 	err = destination.Sync()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
